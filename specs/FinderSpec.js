661336358be717158ca22292b3c23870281c3e10
gedi <gediminas.morkevicius@gmail.com>
2012-03-07 23:53:03 +0200

    add jasmine spec BDD

---
+++ specs/FinderSpec.js
@@ -0,0 +1,11 @@
+describe('Finder', function() {
+  var shape;
+
+  beforeEach(function() {
+    // create some elements
+  });
+
+  it('should be in the right galaxy', function() {
+    expect(true).toBeTrue();
+  });
+});
