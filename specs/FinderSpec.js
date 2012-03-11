af739ff5fa823d3e3c7c5a01c8c97de5a03bf44b
gedi <gediminas.morkevicius@gmail.com>
2012-03-12 00:37:42 +0200

    begin to add specs for functional tests

--- specs/FinderSpec.js
+++ specs/FinderSpec.js
@@ -1,11 +1,17 @@
-describe('Finder', function() {
-  var shape;
+describe('Blaze element finder', function() {
+  var paper;
 
   beforeEach(function() {
-    // create some elements
+    // initial raphael paper
+    paper = Raphael('drawing', 300, 200);
+    // draw few shapes
+    paper.rect(10, 10, 280, 180).attr('stroke', 'black').data('name', 'rect-outer');
+    paper.rect(40, 40, 120, 80).attr('stroke', 'red').data('name', 'rect-inner');
+    paper.circle(90, 90, 20).attr({stroke: 'red', fill: 'blue'}).data('name', 'circle');
   });
 
-  it('should be in the right galaxy', function() {
-    expect(true).toBeTrue();
+  it('should be able to find elements by attribute', function() {
+    var elementSet = blaze(paper).findAllByAttr('stroke', 'red');
+    expect(elementSet.length).toEqual(2);
   });
 });
